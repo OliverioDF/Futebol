354e51ae6abfe9e0eebfde7d7574d8127560da21
OliverioDF <oliveriodf@gmail.com>
2024-09-09 17:48:05 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,25 @@
+let dados = [ 
+        {
+            titulo : "Lucas Piton",
+            descricao : "Lucas Piton é um lateral-esquerdo brasileiro que se destaca por sua velocidade e habilidade nos cruzamentos. Revelado pelo Corinthians,o jogador atualmente defende as cores do Vasco da Gama.",
+            link : "https://pt.wikipedia.org/wiki/Lucas_Piton",
+            tags: "melhor assistência"
+        },
+        {
+            titulo : "Pablo Vegetti",
+            descricao : "Pablo Vegetti é um atacante argentino que se destaca por sua força física e habilidade dentro da área. Conhecido por seus apelidos \"Pirata\", \"El Toro\" e \"Vegeta\", o jogador argentino tem uma longa trajetória no futebol,com passagens por diversos clubes. Atualmente, defende as cores do Vasco da Gama.",
+            link : "https://pt.wikipedia.org/wiki.org/wiki/Pablo_Vegetti",
+            tags: "artilheiro capitao"
+        },
+        {
+            titulo : "Léo Jardim",
+            descricao : "Léo Jardim é um goleiro brasileiro que se destaca por suas defesas difíceis e segurança sob as traves. O jogador tem uma longa trajetória no futebol brasileiro e atualmente defende as cores do Vasco da Gama.",
+            link : "https://pt.wikipedia.org/wiki/Léo_Jardim",
+            tags: "defesas goleirão"
+        }
+    ]
+    
+
+
+
+   
